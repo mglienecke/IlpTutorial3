5fb968f8fe62212a92cd3014db9b6c0743aa25fb
mglienecke <michael@glienecke.de>
2024-10-15 11:01:28 +0100

    added test interfaces

---
+++ src/main/java/uk/ac/ed/inf/ilptutorial3/TutorialInterface.java
@@ -0,0 +1,18 @@
+package uk.ac.ed.inf.ilptutorial3;
+
+public interface TutorialInterface {
+    /**
+     * echo the input string
+     * @param input
+     * @return the input as echo
+     */
+    String echo(String input);
+
+    /**
+     * perform a summing
+     * @param p1
+     * @param p2
+     * @return the sum of p1 and p2
+     */
+    int sum(int p1, int p2);
+}
